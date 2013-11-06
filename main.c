7b2bb97bc2a8e444443c283700663553e34d1ae7
Noel Nicolas <c15noel.nicolas@usafa.edu>
2013-11-05 18:52:10 -0700

    added button functionality through lesson 27 code and clock code with lesson 28

--- main.c
+++ main.c
@@ -12,16 +12,20 @@ int main(void)
 {
 		WDTCTL = WDTPW|WDTHOLD;
 
-	    configureP1PinAsButton(BIT1|BIT2|BIT3|BIT4);         // configure pins 1, 2, and 3 as buttons
 
-	    P1DIR |= BIT0|BIT6;                             // set launchpad LEDs to output
 
+
+
+	    initSPI();
+	    LCDinit();
+	    LCDclear();
 
         unsigned char player = initPlayer();
 
         init_timer();
         init_buttons();
         __enable_interrupt();
+
 
         while(1)
         {
@@ -72,10 +76,34 @@ int main(void)
 
 void init_timer()
 {
-        // do timer initialization work
+	    TACTL &= ~(MC1|MC0);        // stop timer
+
+	    TACTL |= TACLR;             // clear TAR
+
+	    TACTL |= TASSEL1;           // configure for SMCLK - what's the frequency (roughly)?
+
+	    TACTL |= ID1|ID0;           // divide clock by 8 - what's the frequency of interrupt?
+
+	    TACTL &= ~TAIFG;            // clear interrupt flag
+
+	    TACTL |= MC1;               // set count mode to continuous
+
+	    TACTL |= TAIE;              // enable interrupt
+
+
+
 }
 
 void init_buttons()
-{
+{	configureP1PinAsButton(BIT1|BIT2|BIT3|BIT4);
+
+    P1DIR &= ~(BIT1|BIT2|BIT3|BIT4);                // set buttons to input
+    P1IE |= BIT1|BIT2|BIT3|BIT4;                 // enable the interrupts
+    P1IES |= BIT1|BIT2|BIT3|BIT4;                   // configure interrupt to sense falling edges
+    P1REN |= BIT1|BIT2||BIT4;                   // enable internal pull-up/pull-down network
+    P1OUT |= BIT1|BIT2|BIT3|BIT4;                   // configure as pull-up
+    P1IFG &= ~(BIT1|BIT2|BIT3);                // clear flags
+
+}
         // do button initialization work
 }
